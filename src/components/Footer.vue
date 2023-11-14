3fb9873d6f0b9c4cee6960619e724c8d0be111f6
Dylan McDonald <dylan@deepdivedylan.io>
2023-11-13 21:53:51 -0800

    added social icons to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="tw-bg-indigo-600 tw-bottom-0 tw-sticky">
-    <footer class="tw-container tw-px-6 tw-py-8 tw-mx-auto tw-text-xl tw-font-bold tw-text-gray-100">
-      This is a footer
+    <footer class="tw-container tw-flex tw-items-center tw-px-6 tw-py-8 tw-mx-auto tw-text-xl tw-font-bold tw-text-gray-100">
+      <div class="tw-pr-2">
+        <a href="https://github.com/deepdivedylan"><img class="tw-h-5" src="/github.svg" alt="" /></a>
+      </div>
+      <div class="tw-pr-6">
+        <a href="https://github.com/deepdivedylan">deepdivedylan</a>
+      </div>
+      <div class="tw-pr-2">
+        <a href="https://x.com/deepdivedylan">&#x1D54F;</a>
+      </div>
+      <div>
+        <a href="https://x.com/deepdivedylan">@deepdivedylan</a>
+      </div>
     </footer>
   </div>
 </template>
-<script setup>
-</script>
